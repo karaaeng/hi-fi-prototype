86beffd4a7a34ed988bd7685fca89b8d24925052
Cat Davis <catriandavis@gmail.com>
2021-03-10 12:19:50 -0800

    addfriends syntax

--- App/Components/addFriends.js
+++ App/Components/addFriends.js
@@ -10,11 +10,12 @@ import {
 } from '@expo-google-fonts/comfortaa';
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Dimensions, ScrollView} from 'react-native';
 import { Images } from '../Themes';
 
 export default function AddFriends({navigation}) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedAll, setSelected] = useState(false);
 
   let [fontsLoaded] = useFonts({
     Comfortaa_300Light,
@@ -23,6 +24,30 @@ export default function AddFriends({navigation}) {
     Comfortaa_600SemiBold,
     Comfortaa_700Bold,
   });
+
+  function contact(name) {
+    return (
+      <TouchableOpacity style = {styles.contactButton} onPress = { () => {
+        console.log(selectedAll); 
+      }}>
+      <View style = {styles.contactText}>
+        <Text style = {styles.buttonText}> {name} </Text>
+        <Icon name="plus" style={styles.icon}/>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  function addAll() {
+    return (
+      <TouchableOpacity style = {styles.addButton} onPress = { () => {
+        setSelected(true); 
+      }}>
+        <Text style = {styles.addText}> add all </Text>
+
+      </TouchableOpacity>
+    );
+  }
 
     return(
       <View style={styles.container}>
@@ -35,7 +60,25 @@ export default function AddFriends({navigation}) {
             </View>
             <Text style={styles.addAllbutton}>add all</Text>
         </View>
-        
+        <View style = {styles.othercontainer}>
+        {addAll()}
+        <ScrollView>
+            {contact("Marie Burnett")}
+            {contact("George Burnside")}
+            {contact("Kara Eng")}
+            {contact("Isa Fulford")}
+            {contact("Alex Hennessey")}
+            {contact("Nat Hojel")}
+            {contact("Xa Koch")}
+            {contact("Elianna Knight")}
+            {contact("Ryan Varis")}
+            {contact("Lauren Yi")}
+            {contact("Cam Burton")}
+            {contact("Thom Henri")}
+            {contact("Pablo Ocampo")}
+      </ScrollView>
+      
+    </View>
         <View>
           <TouchableOpacity onPress={() => navigation.navigate('Ready')}>
         <Image style = {styles.forward} source={Images.forward_icon}/>
@@ -50,6 +93,12 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         backgroundColor: 'white',
+      },
+      othercontainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignContent: 'center', 
+        justifyContent: 'space-between'
       },
       topBar: {
         flex: 1,
@@ -89,5 +138,41 @@ const styles = StyleSheet.create({
         fontSize: 28,
         marginTop: 20,
         marginLeft: 20,
+      },
+      addButton: {
+        marginTop: 100,
+        alignItems: "center",
+        alignSelf: 'flex-end', 
+        padding: 10,
+        height: 50,
+        width: 150,
+        marginRight: 47,
+        backgroundColor: "#FFF0C1",
+        borderRadius: 30,
+      },
+    
+      addText: {
+        fontSize: 20,
+        color: '#939393',
+      },
+      contactButton: {
+        marginTop: 20,
+        alignItems: "center",
+        borderRadius: 30,
+        padding: 20,
+        height: 73,
+        width: 336,
+        alignSelf: 'center',
+        backgroundColor: "#FFF0C1",
+      },
+      contactText: {
+        flexDirection: 'row', 
+        alignItems: 'baseline',
+        color: '#939393',
+      },
+      buttonText: {
+        fontFamily: 'Comfortaa_700Bold',
+        fontSize: 30,
+        color: '#939393',
       },
 });
